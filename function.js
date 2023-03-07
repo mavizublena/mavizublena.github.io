fe643fe22c847d8b97a1ac467d655bf23708ae66
mavi zublena <mavizu28@hotmail.com>
2023-03-07 09:26:23 -0300

    mi primer repositorio

---
+++ function.js
@@ -0,0 +1,13 @@
+
+window.onload = iniciar;
+
+function iniciar (){
+  var button = document.getElementById("button");
+  button.addEventListener ("click", clickButton);
+}
+
+function clickButton() {
+    alert ("GRACIAS!")
+
+}
+
